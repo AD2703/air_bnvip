a889e96dabf41103a406893ba83bf35a9eb1a6b4
maximedecaritat <mdecaritat@gmail.com>
2021-11-15 17:32:08 +0100

    Add dotenv - Protect my secret data in .env file

--- app/controllers/houses_controller.rb
+++ app/controllers/houses_controller.rb
@@ -1,3 +1,19 @@
 class HousesController < ApplicationController
 #controller
+
+def new
+@house = Houses.new
+end
+
+def create
+  house = Houses.new(house_params)
+  house.save
+  redirect_to house_path
+end
+
+
+def house_params
+    params.require(:house).permit(:users_id, :name, :description)
+end
+
 end
