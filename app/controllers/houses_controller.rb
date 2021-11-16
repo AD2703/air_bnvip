bd49e11b28e0950626bf9c09b8f80926ad4bddde
Alejandro Doria <alexdoria.7@gmail.com>
2021-11-16 11:21:00 +0100

    update

--- app/controllers/houses_controller.rb
+++ app/controllers/houses_controller.rb
@@ -2,7 +2,6 @@ class HousesController < ApplicationController
   def index
     @houses = House.all
   end
-  # controller
 
   def show
     @house = House.find(params[:id])
@@ -16,15 +15,31 @@ class HousesController < ApplicationController
     @house = House.new(house_params)
     @house.user = current_user
     if @house.save
-      redirect_to houses_path
+      redirect_to house_path(@house)
     else
       render :new
     end
   end
 
+  def edit
+    @house = House.find(params[:id])
+  end
+
+  def update
+    @house = House.find(params[:id])
+    @house.update(house_params)
+    redirect_to house_path(@house)
+  end
+
+  def destroy
+    @house = House.find(params[:id])
+    @house.destroy
+    redirect_to house_path
+  end
+
   private
 
   def house_params
-    params.require(:house).permit(:name, :description, photos: [])
+    params.require(:house).permit(:name, :description, :price, :address, :country, photos: [])
   end
 end
