ab7c22e4ae06462cc03ffb645188f471a011f962
Alejandro Doria <alexdoria.7@gmail.com>
2021-11-16 09:43:33 +0100

    update show controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
+  resources :houses
   resources :bookings, only: %i[create new show index destroy]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
