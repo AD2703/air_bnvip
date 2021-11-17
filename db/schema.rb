d177fc7648da1e43f823db97ada587cc28d0c69f
Alejandro Doria <alexdoria.7@gmail.com>
2021-11-17 10:53:33 +0100

    migrations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_16_084355) do
+ActiveRecord::Schema.define(version: 2021_11_17_093311) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -42,6 +42,8 @@ ActiveRecord::Schema.define(version: 2021_11_16_084355) do
     t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.date "start_date"
+    t.date "end_date"
     t.index ["house_id"], name: "index_bookings_on_house_id"
     t.index ["user_id"], name: "index_bookings_on_user_id"
   end
