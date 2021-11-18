4b0c5e6869606036a73d51ee4dd1aebaaabc3c7c
carles espunya <carlespuna@gmail.com>
2021-11-18 16:44:13 +0100

    button style form

--- app/javascript/plugins/flatpickr.js
+++ app/javascript/plugins/flatpickr.js
@@ -2,11 +2,13 @@ import flatpickr from "flatpickr";
 
 const initFlatpickr = () => {
   const dates = document.querySelector(".date-booked");
+  if (dates) {
   flatpickr(".datepicker", {
     mode: "range",
     minDate: "today",
     disable: JSON.parse(dates.dataset.dates),
   });
+  }
 }
 
 export { initFlatpickr };
--- app/javascript/plugins/init_star_rating.js
+++ app/javascript/plugins/init_star_rating.js
@@ -9,7 +9,6 @@ const initStarRating = () => {
     });
 
   }
-
 };
 
 export { initStarRating }
