efc41342098079782e184b16d190ab2cbad98db8
maximedecaritat <mdecaritat@gmail.com>
2021-11-18 12:06:10 +0100

    reviews working

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,4 +1,5 @@
 class Booking < ApplicationRecord
   belongs_to :house
   belongs_to :user
+  has_many :reviews
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,9 @@ Rails.application.routes.draw do
     resources :bookings, only: %i[create show]
   end
   get "dashboard", to: "pages#dashboard"
+
+  resources :bookings do
+   resources :reviews, only: %i[new create]
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
