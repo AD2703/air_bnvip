177eac0061e76543c6c80069b55f41124255dd0b
carles espunya <carlespuna@gmail.com>
2021-11-18 11:45:24 +0100

    myhouses

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,6 @@ Rails.application.routes.draw do
     resources :bookings, only: %i[create show]
   end
   get "dashboard", to: "pages#dashboard"
+  get "my_houses", to: "pages#my_houses"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
