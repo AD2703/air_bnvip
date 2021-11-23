72db338faad1af73094203b014174e92eeabfbe9
Alejandro Doria <alexdoria.7@gmail.com>
2021-11-23 16:15:27 +0100

    identation

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
     resources :bookings, only: %i[create show]
   end
   resources :bookings do
-   resources :reviews, only: %i[new create]
+    resources :reviews, only: %i[new create]
   end
   get "dashboard", to: "pages#dashboard"
   get "my_houses", to: "pages#my_houses"
